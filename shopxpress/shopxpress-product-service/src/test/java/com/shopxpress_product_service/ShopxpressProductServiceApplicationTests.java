3382de2bcbe8fc7ef5c420d8ca1811498d7a6c20
emreirgoren <emreirgoren@outlook.com>
2024-09-12 15:13:30 +0300

    Project Initialized.

---
+++ shopxpress/shopxpress-product-service/src/test/java/com/shopxpress_product_service/ShopxpressProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopxpress_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopxpressProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
